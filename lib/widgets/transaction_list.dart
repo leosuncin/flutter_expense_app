fd9d851cf411ad96aeae1fef6cccce1c301a9e12
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-05-22 14:45:26 -0600

    Making the list scrollable

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,51 +10,56 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _transactions
-          .map((transaction) => Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${transaction.amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 75,
+      child: SingleChildScrollView(
+        child: Column(
+          children: _transactions
+              .map((transaction) => Card(
+                    child: Row(
                       children: <Widget>[
-                        Text(
-                          transaction.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          )),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$${transaction.amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
                           ),
                         ),
-                        Text(
-                          DateFormat.yMMMd().format(transaction.date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              transaction.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              DateFormat.yMMMd().format(transaction.date),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-              ))
-          .toList(),
+                    ),
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
